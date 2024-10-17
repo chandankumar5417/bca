b823b6a1d81bca8af8eef53c7f3dc4b0d30b2d3d
premsagar649 <ppremsagar649@gmail.com>
2024-10-17 13:37:41 +0530

    first commit

---
+++ clone/index.js
@@ -0,0 +1,33 @@
+// document.write("swift")
+// let v = 5 
+// v = 8
+// console.log(v)
+// hey = 40
+// console.log ( typeof(hey))
+// const k = 10
+// k = 30
+// console.log(k)
+
+// if(word===40){
+//     console.log("my value is ",word)
+
+// }else if ( hello===12){
+// console.log ("my value is ",hello)
+// }
+for (let i=0; i<20;i++){
+    console.log ("i =",i)
+}
+
+// let i=0;
+// while(i===0){
+//     console.log("i'm while loop")
+// // }
+
+// const arr=[0,1,2,3];
+// console.log(arr);
+// console.log(typeof(arr[0]));
+if (5==5){
+    const i = 5
+}
+8O000000000000000
+    
